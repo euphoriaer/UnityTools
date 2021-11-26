7248797575df479d694a8502886a5a46a308a5f3
panbin <1764183867@qq.com>
2021-11-26 18:39:00 +0800

    Fix

--- Assets/Tools/Editor/ToolsSettings/AnimationSpreat.cs
+++ Assets/Tools/Editor/ToolsSettings/AnimationSpreat.cs
@@ -3,49 +3,48 @@ using UnityEngine;
 
 public class AnimationSpreat : AssetPostprocessor
 {
-
-    //void OnPreprocessModel()
-    //{
-    //    if (assetPath.Contains("@"))
-    //    {
-    //        ModelImporter modelImporter = assetImporter as ModelImporter;
-    //        modelImporter.materialImportMode = ModelImporterMaterialImportMode.None;
-
-    //        //动画禁止材质导入
-    //        modelImporter.materialImportMode = ModelImporterMaterialImportMode.None;
-    //        //设置为Humannoid
-    //        modelImporter.animationType = ModelImporterAnimationType.Human;
-    //        //创建Avata
-    //        modelImporter.avatarSetup = ModelImporterAvatarSetup.CreateFromThisModel;
-    //    }
-    //}
+    private void OnPreprocessModel()
+    {
+        if (assetPath.Contains(ToolsSettings.Instance.Mark))
+        {
+            ModelImporter modelImporter = assetImporter as ModelImporter;
+            modelImporter.materialImportMode = ModelImporterMaterialImportMode.None;
+            //动画禁止材质导入
+            modelImporter.materialImportMode = ModelImporterMaterialImportMode.None;
+            //设置为Humannoid
+            modelImporter.animationType = ModelImporterAnimationType.Human;
+            //创建Avata
+            modelImporter.avatarSetup = ModelImporterAvatarSetup.CreateFromThisModel;
+        }
+    }
 
     private void OnPostprocessModel(GameObject g)
     {
-        Debug.Log("动画分离2" + assetPath+"   "+ g.name);
-
-        if (assetPath.Contains(ToolsSettings.Instance.Mark))
+        string cilpName = g.name + ".anim";
+        Debug.Log("动画分离2" + assetPath + "   " + cilpName);
+        EditorApplication.delayCall += () =>
         {
-
-            //copy 动画
-            var assets = AssetDatabase.LoadAllAssetRepresentationsAtPath(assetPath);
-
-
-            foreach (var obj in assets)
+            if (assetPath.Contains(ToolsSettings.Instance.Mark))
             {
-                Debug.Log(obj.name);
+                //copy 动画
+                var assets = AssetDatabase.LoadAllAssetRepresentationsAtPath(assetPath);
+
+                foreach (var obj in assets)
+                {
+                    Debug.Log(obj.name);
+                }
+
+                AnimationClip clip = AssetDatabase.LoadAssetAtPath(assetPath, typeof(AnimationClip)) as AnimationClip;
+
+                var newClip = UnityEngine.Object.Instantiate(clip);
+                AssetDatabase.CreateAsset(newClip, "Assets/Resources/动画" + "/" + cilpName);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
             }
-
-            AnimationClip clip = AssetDatabase.LoadAssetAtPath(assetPath, typeof(AnimationClip)) as AnimationClip;
-
-            var newClip = UnityEngine.Object.Instantiate(clip);
-            AssetDatabase.CreateAsset(newClip, ToolsSettings.Instance.Anim);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-        }
-        else
-        {
-            Debug.Log("导入模型，创建控制器" + assetPath);
-        }
+            else
+            {
+                Debug.Log("导入模型，创建控制器" + assetPath);
+            }
+        };
     }
 }
